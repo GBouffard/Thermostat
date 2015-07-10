b675f91c1e458b6d32b5527fed453107de565a90
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-07-10 19:29:41 +0100

    wrote tests

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-:sunny: Thermostat :umbrella:
+:high_brightness: Thermostat :high_brightness:
 ===
 This is me re-doing from scratch the Makers Academy Thermostat tutorial to learn about Javascript and JQuery.
+
+
+- Thermostat starts at 20 degrees
+- You can increase the temp with the up button
+- You can decrease the temp with the down button
+- The minimum temperature is 10 degrees
+- If power saving mode is on, the maximum temperature is 25 degrees
+- If power saving mode is off, the maximum temperature is 32 degrees
+- Power saving mode is on by default
+- You can reset the temperature to 20 by hitting  the reset button
+- The thermostat should colour the display based on energy usage - < 18 is green, < 25 is yellow, otherwise red
 
 Heroku
 ----
--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,2 +1,50 @@
-describe("Thermostat", function() {
+describe('Thermostat', function() {
+  xit('starts at a temperature of 20 degrees', function(){
+
+  });
+
+  xit('has a temperature of 22 when the up function is pressed twice', function(){
+
+  });
+
+  xit('has a temperature of 18 when the down function is pressed twice', function(){
+    
+  });
+
+  xit('has a minimum temperature of 10 degrees', function(){
+    
+  });
+
+  xit('has a power saving mode, that is on by default', function(){
+    
+  });
+
+
+  xit('has a toggle function to switch the power saving mode on and off', function(){
+    
+  });
+
+  xit('if the power saving mode is on, it has a maximum temperature of 25 degrees', function(){
+    
+  });
+
+  xit('if the power saving mode is off, it has a maximum temperature of 32 degrees', function(){
+    
+  });
+
+  xit('has a reset function to bring the temperature back to 20 degrees', function(){
+    
+  });
+
+  xit('displays information in yellow at initial state', function(){
+    
+  });
+
+  xit('displays information in green when the temperature is under 18 degrees', function(){
+    
+  });
+
+  xit('displays information in red when the temperature is over 25 degrees', function(){
+    
+  });
 });
