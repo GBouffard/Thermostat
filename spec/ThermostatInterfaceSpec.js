bced3e03546e865736c720d5210d7175afc5e23a
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-07-12 11:08:14 +0100

    wrote all the needed JQuery tests

--- spec/ThermostatInterfaceSpec.js
+++ spec/ThermostatInterfaceSpec.js
@@ -7,4 +7,31 @@ describe('Thermostat interface', function() {
     loadFixtures('index.html');
     thermostat = new Thermostat
   });
+
+  xit('displays the temperature at 20 by default', function(){
+  });
+
+  xit('can increase the temperature by clicking the + button', function(){
+  });
+
+  xit('can decrease the temperature by clicking the - button', function(){
+  });
+
+  xit('displays the power saving as On by default', function(){
+  });
+
+  xit('can toggle the power saving On and Off with the Power Saving Button', function(){
+  });
+
+  xit('brings back the temperature to 20 when we press the Reset button', function(){
+  });
+
+  xit('displays the temperature in Yellow by default', function(){
+  });
+
+  xit('displays the temperature in Green when the temperature is under 18 degrees', function(){
+  });
+
+  xit('displays the temperature in Red when the temperature is over 25 degrees', function(){
+  });
 });
