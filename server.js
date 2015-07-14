c51676fa0ffd458f4aec25b5d763088bdbb8a70a
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-07-14 16:21:22 +0100

    created a Node server

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use("/public", express.static(__dirname + '/public'));
+app.use("/src", express.static(__dirname + '/src'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
